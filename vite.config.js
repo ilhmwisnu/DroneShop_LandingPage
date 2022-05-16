7af884a6250acba72569da457d2025f8045e2b4c
ilhmwisnu <ilhamwisnu42@gmail.com>
2022-05-16 23:21:33 +0700

    small change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/DroneShop_LandingPage/',
   plugins: [vue()],
   resolve: {
     alias: {
